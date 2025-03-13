e7c4d9969dd33fd990668b5f6147a11cd3e68717
Blaise Pabon <blaise@gmail.com>
2025-03-13 19:11:46 +0000

    WIP fixing #12 Add some changes to pages
    fixing issue #12
    
    Co-authored-by: Daniel Williams <clearlydaniel@gmail.com>

--- mysite/conf.py
+++ mysite/conf.py
@@ -575,7 +575,7 @@ HIDDEN_AUTHORS = ['Guest']
 # Final location for the main blog page and sibling paginated pages is
 # output / TRANSLATION[lang] / INDEX_PATH / index-*.html
 # (translatable)
-INDEX_PATH = ""
+INDEX_PATH = "mysite"
 
 # Optional HTML that displayed on “main” blog index.html files.
 # May be used for a greeting. (translatable)
