fde3c638cbf0dbc67a597df92af11fc127b80a98
Blaise Pabon <blaise@gmail.com>
2025-03-20 19:21:34 +0000

    Change theme to
    Install bnw theme to use the `bnw` theme:
    ```
    nikola theme -i bnw
    ```
    
    Edit settings in `conf.py`
    ```
    THEME="bnw"
    USE_BUNDLES = True
    ```

--- project-site/conf.py
+++ project-site/conf.py
@@ -1342,7 +1342,8 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 # Bundle JS and CSS into single files to make site loading faster in a HTTP/1.1
 # environment but is not recommended for HTTP/2.0 when caching is used.
 # Defaults to True.
-# USE_BUNDLES = True
+# Pre-built CSS files are only included when using bundles.
+USE_BUNDLES = True
 
 # Plugins you don't want to use. Be careful :-)
 # DISABLED_PLUGINS = ["render_galleries"]
